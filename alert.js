29414c0b1a57a3c428fa564bc1022cb960739d39
Will Kencel <72818086+wkencel@users.noreply.github.com>
2020-12-09 22:35:10 -0500

    Add files via upload
    "starter files"

---
+++ alert.js
@@ -0,0 +1,7 @@
+alert('code time')
+
+// chrome.runtime.onInstalled.addListener(function() {
+//     chrome.storage.sync.set({color: '#3aa757'}, function() {
+//       console.log("The color is green.");
+//     });
+//   });
